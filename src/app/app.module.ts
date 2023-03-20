270d2c102380fbf32204fe183c5c0acbc2d55a59
José Neto <ribeirojoseph44@gmail.com>
2023-03-20 09:40:31 -0300

    Global CSS Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // All components, directives and pipes
   declarations: [
     AppComponent
   ],
+
+  // Modules (inner app or external)
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  // Services
   providers: [],
+
+  // First app render
   bootstrap: [AppComponent]
 })
 export class AppModule { }
